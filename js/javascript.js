65d0723353beb5e8ac18cecfa1cba68788fc5b1d
AnthonyJuho <112909624+AnthonyJuho@users.noreply.github.com>
2023-05-29 11:02:54 +0900

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+var container = document.getElementById('container');
+var screenl = document.getElementById('screen');
+var width = screenl.offsetWidth;
+var height = screenl.offsetHeight;
+container.style.width = width+"px";
+container.style.height = height+"px";
